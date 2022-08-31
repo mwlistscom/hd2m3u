fe82dd284784adfbfe4378fbdab4bea556615e19
John Martin <7551071+mwlistscom@users.noreply.github.com>
2022-08-31 10:05:45 -0600

    Update hd2m3u.php

--- hd2m3u.php
+++ hd2m3u.php
@@ -8,8 +8,8 @@
  * @link        https://rockmym3u.com
  *
  * hd2m3u is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- * GetVod is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with GetVod. If not, see http://www.gnu.org/licenses/.
+ * hd2m3u is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with hd2m3u. If not, see http://www.gnu.org/licenses/.
  */
 
 //
